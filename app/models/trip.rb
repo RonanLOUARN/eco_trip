70c01df25ac89851816d6991f76aec554d7dfeec
Ronan Louarn <ronan33720@hotmail.com>
2019-07-08 18:53:22 +0200

    Add enum 'state' to Trip model

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -1,4 +1,10 @@
 class Trip < ApplicationRecord
   validates :state, presence: true
   validates :code, uniqueness: true, presence: true
+
+  enum state: {
+    created: 'created',
+    started: 'started',
+    canceled: 'canceled'
+  }
 end
