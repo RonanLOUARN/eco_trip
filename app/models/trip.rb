cb505ec95d511b2a9fdb29d08a61524b9fec7cb3
Ronan Louarn <ronan33720@hotmail.com>
2019-07-08 18:54:06 +0200

    Add method to generate uniq code

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -2,9 +2,26 @@ class Trip < ApplicationRecord
   validates :state, presence: true
   validates :code, uniqueness: true, presence: true
 
+  before_validation :set_code
+
   enum state: {
     created: 'created',
     started: 'started',
     canceled: 'canceled'
   }
+
+  private
+
+  def set_code
+    return if persisted?
+
+    self.code = generate_code
+  end
+
+  def generate_code
+    loop do
+      new_code = SecureRandom.base58(4)
+      break new_code unless Trip.find_by(code: new_code)
+    end
+  end
 end
