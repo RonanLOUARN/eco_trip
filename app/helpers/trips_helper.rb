d02a571fae83907a07538450afaeb11c4a815ac0
Ronan Louarn <ronan33720@hotmail.com>
2019-07-08 21:06:08 +0200

    Add trips index view

---
+++ app/helpers/trips_helper.rb
@@ -0,0 +1,16 @@
+module TripsHelper
+  def trip_text_color(trip)
+    'text-danger' if trip.canceled?
+  end
+
+  def display_links(trip)
+    return if trip.canceled?
+
+    if trip.started?
+      link_to 'Cancel', trip, method: :delete, class: 'btn btn-danger', remote: true
+    else
+      link_to('Start', trip, method: :patch, class: 'btn btn-warning mx-1', id: "start-trip-btn-#{trip.id}", remote: true) +
+      link_to('Cancel', trip, method: :delete, class: 'btn btn-danger', id: "cancel-trip-btn-#{trip.id}", remote: true)
+    end
+  end
+end
